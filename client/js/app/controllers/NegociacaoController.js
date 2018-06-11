0f011e25981a89d62a0f079ef4a1bd8cb58ad195
Bruno Cicero Rodrigues de Souza <bruno.rodrigues@venturus.org.br>
2018-06-11 08:12:08 -0300

    Associando metodos do controller as acoes do usuario

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,5 +3,14 @@ class NegociacaoController {
     adicionar(event) {
         event.preventDefault();
         alert("Chamei a acao no controller");
+
+        let $ = document.querySelector.bind(document);
+        let inputQuantidade = $('#data');
+        let inputData = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputQuantidade.value);
+        console.log(inputData.value);
+        console.log(inputValor.value);
     }
 }
