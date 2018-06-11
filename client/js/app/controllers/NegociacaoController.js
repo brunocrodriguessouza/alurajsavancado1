993063e06627f78228c8e617f48ca7775ecab453
Bruno Cicero Rodrigues de Souza <bruno.rodrigues@venturus.org.br>
2018-06-11 14:56:22 -0300

    Criando um ListModel

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,24 +1,37 @@
 class NegociacaoController {
 
     constructor() {
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
     
     adicionar(event) {
+
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adicionar(this._criarNegociacao());
+        this._limparFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criarNegociacao() {
+
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value), 
             this._inputQuantidade.value,
-            this._inputValor.value,
-        )
+            this._inputValor.value)
+    }
 
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
-     
-        // adicionar a negociacao em uma lista
+    _limparFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputQuantidade.focus();
     }
 }
