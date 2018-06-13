11ab73c6cc8267dc9ee2d59b3083f0ab7bf62c60
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-06-12 22:27:45 -0300

    Requisicoes Ajax com o objeto XMLHttpRequest e condicao com status da requisicao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -30,6 +30,38 @@ class NegociacaoController {
         this._limparFormulario();
     }
 
+    importarNegociacoes() {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+
+        /*
+            0: requisicao ainda nao iniciada
+            1: conexao com o servidor estabelecida
+            2: requisicao recebida
+            3: processando requisicao
+            4: requisicao concluida e a resposta pronta
+        */
+
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4) {
+                
+                if(xhr.status == 200) {
+                    console.log('Obtendo as negociacoes do servidor');
+                } else {
+                    console.log("Nao foi possivel obter as negociacoes do servidor");
+                }
+            }
+
+        };
+
+        xhr.send();
+
+
+    }
+
     removerTodas() {
 
         this._listaNegociacoes.removerTodas();
